56404bb571e9938fc44320a2b2cce94368d8b1e2
ilyamusabirov <ilya@musabirov.info>
2026-02-22 18:56:55 -0800

    Demo version

---
+++ app.py
@@ -0,0 +1,26 @@
+from shiny import App, render, ui
+
+app_ui = ui.page_sidebar(
+    ui.sidebar(
+        ui.input_slider("n", "Number of observations", min=1, max=100, value=50),
+    ),
+    ui.card(
+        ui.output_text("build"),
+    ),
+    ui.card(
+        ui.output_text("txt"),
+    ),
+    title="Minimal Shiny App - Main branch",
+)
+
+
+def server(input, output, session):
+    @render.text
+    def build():
+        return f"stable version"
+    @render.text
+    def txt():
+        return f"Number of observations: {input.n()}"
+
+
+app = App(app_ui, server)
